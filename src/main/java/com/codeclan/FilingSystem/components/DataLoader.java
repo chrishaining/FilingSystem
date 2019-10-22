6844840289dfcd6c8fdefb42f51d471b8b28a6d6
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-22 20:57:03 +0100

    adds Folder class

--- src/main/java/com/codeclan/FilingSystem/components/DataLoader.java
+++ src/main/java/com/codeclan/FilingSystem/components/DataLoader.java
@@ -1,7 +1,9 @@
 package com.codeclan.FilingSystem.components;
 
 import com.codeclan.FilingSystem.models.File;
+import com.codeclan.FilingSystem.models.Folder;
 import com.codeclan.FilingSystem.repositories.FileRepository;
+import com.codeclan.FilingSystem.repositories.FolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,6 +15,9 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     FileRepository fileRepository;
 
+    @Autowired
+    FolderRepository folderRepository;
+
     public void run(ApplicationArguments args) {
 
         File file1 = new File("Drawing", ".io", 32.5);
@@ -20,6 +25,9 @@ public class DataLoader implements ApplicationRunner {
 
         File file2 = new File("Guernica", ".jpeg", 30);
         fileRepository.save(file2);
+
+        Folder folder1 = new Folder("Artwork");
+        folderRepository.save(folder1);
 
     }
 }
---
+++ src/main/java/com/codeclan/FilingSystem/repositories/FolderRepository.java
@@ -0,0 +1,9 @@
+package com.codeclan.FilingSystem.repositories;
+
+import com.codeclan.FilingSystem.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
