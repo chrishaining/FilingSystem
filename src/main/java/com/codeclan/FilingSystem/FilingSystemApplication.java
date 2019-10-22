c55be0d84fed20ca9192658200a9a541f19d47a9
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-22 16:15:38 +0100

    creates File class

---
+++ src/main/java/com/codeclan/FilingSystem/FilingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.FilingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/FilingSystem/FilingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.FilingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
